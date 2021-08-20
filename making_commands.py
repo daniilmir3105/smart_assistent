e3d2adb0ecf98c58a15bc4895a53ca53404e8d38
Daniil <miroshnichienko.daniil@gmail.com>
2021-08-20 17:01:57 +0300

    make documentation in making_commands.py

--- making_commands.py
+++ making_commands.py
@@ -80,8 +80,8 @@ class Making:
 
     def search_for_video_on_youtube(self, translator, *args: tuple):
         """
-        Поиск видео на YouTube с автоматическим открытием ссылки на список результатов
-        :param args: фраза поискового запроса
+        Search for videos on YouTube with automatic opening of a link to the list of results
+        :param args: search query phrase
         """
         if not args[0]: return
         search_term = " ".join(args[0])
@@ -91,17 +91,17 @@ class Making:
 
     def search_for_definition_on_wikipedia(self, translator, assistant, *args: tuple):
         """
-        Поиск в Wikipedia определения с последующим озвучиванием результатов и открытием ссылок
-        :param args: фраза поискового запроса
+        Search for definitions in Wikipedia, followed by voicing the results and opening links
+        :param args: search query phrase
         """
         if not args[0]: return
 
         search_term = " ".join(args[0])
 
-        # установка языка (в данном случае используется язык, на котором говорит ассистент)
+        # setting the language (in this case, the language spoken by the assistant is used)
         wiki = wikipediaapi.Wikipedia(assistant.speech_language)
 
-        # поиск страницы по запросу, чтение summary, открытие ссылки на страницу для получения подробной информации
+        # search for a page by query, read summary, open a link to the page for detailed information
         wiki_page = wiki.page(search_term)
         try:
             if wiki_page.exists():
@@ -109,17 +109,17 @@ class Making:
                     translator.get("Here is what I found for {} on Wikipedia").format(search_term))
                 webbrowser.get().open(wiki_page.fullurl)
 
-                # чтение ассистентом первых двух предложений summary со страницы Wikipedia
-                # (могут быть проблемы с мультиязычностью)
+                # the assistant reads the first two sentences of summary from the Wikipedia page
+                # (there may be problems with multilingualism)
                 self.__speaking.play_voice_assistant_speech(wiki_page.summary.split(".")[:2])
             else:
-                # открытие ссылки на поисковик в браузере в случае, если на Wikipedia не удалось найти ничего по запросу
+                # opening a link to a search engine in the browser if nothing could be found on Wikipedia for the query
                 self.__speaking.play_voice_assistant_speech(translator.get(
                     "Can't find {} on Wikipedia. But here is what I found on google").format(search_term))
                 url = "https://google.com/search?q=" + search_term
                 webbrowser.get().open(url)
 
-        # поскольку все ошибки предсказать сложно, то будет произведен отлов с последующим выводом без остановки программы
+        # since it is difficult to predict all errors, it will be captured and then output without stopping the program
         except:
             self.__speaking.play_voice_assistant_speech(translator.get("Seems like we have a trouble. See logs for more information"))
             traceback.print_exc()
@@ -127,8 +127,8 @@ class Making:
 
     def get_translation(self, assistant, person, translator, *args: tuple):
         """
-        Получение перевода текста с одного языка на другой (в данном случае с изучаемого на родной язык или обратно)
-        :param args: фраза, которую требуется перевести
+        Getting a translation of a text from one language to another (in this case, from the one being studied to the native language or back)
+        :param args: the phrase that you want to translate
         """
         if not args[0]: return
 
@@ -138,45 +138,45 @@ class Making:
 
         old_assistant_language = assistant.speech_language
         try:
-            # если язык речи ассистента и родной язык пользователя различаются, то перевод выполяется на родной язык
+            # if the assistant's speech language and the user's native language differ, then the translation is performed into the native language
             if assistant.speech_language != person.native_language:
-                translation_result = google_translator.translate(search_term,  # что перевести
-                                                                 src=person.target_language,  # с какого языка
-                                                                 dest=person.native_language)  # на какой язык
+                translation_result = google_translator.translate(search_term,  # what to translate
+                                                                 src=person.target_language,  # from what language
+                                                                 dest=person.native_language)  # in what language
 
                 self.__speaking.play_voice_assistant_speech("The translation for {} in Russian is".format(search_term))
 
-                # смена голоса ассистента на родной язык пользователя (чтобы можно было произнести перевод)
+                # changing the assistant's voice to the user's native language (so that the translation can be pronounced)
                 assistant.speech_language = person.native_language
                 self.__speaking.setup_assistant_voice()
 
-            # если язык речи ассистента и родной язык пользователя одинаковы, то перевод выполяется на изучаемый язык
+            # if the assistant's speech language and the user's native language are the same, then the translation is performed into the language being studied
             else:
-                translation_result = google_translator.translate(search_term,  # что перевести
-                                                                 src=person.native_language,  # с какого языка
-                                                                 dest=person.target_language)  # на какой язык
+                translation_result = google_translator.translate(search_term,  # what to translate
+                                                                 src=person.native_language,  # from what language
+                                                                 dest=person.target_language)  # in what language
                 self.__speaking.play_voice_assistant_speech("По-английски {} будет как".format(search_term))
 
-                # смена голоса ассистента на изучаемый язык пользователя (чтобы можно было произнести перевод)
+                # changing the assistant's voice to the user's language being studied (so that the translation can be pronounced)
                 assistant.speech_language = person.target_language
                 self.__speaking.setup_assistant_voice()
 
-            # произнесение перевода
+            # pronouncing the translation
             self.__speaking.play_voice_assistant_speech(translation_result.text)
 
-        # поскольку все ошибки предсказать сложно, то будет произведен отлов с последующим выводом без остановки программы
+        # since it is difficult to predict all errors, it will be captured and then output without stopping the program
         except:
             self.__speaking.play_voice_assistant_speech(translator.get("Seems like we have a trouble. See logs for more information"))
             traceback.print_exc()
 
         finally:
-            # возвращение преждних настроек голоса помощника
+            # return to the previous settings of the assistant's voice
             assistant.speech_language = old_assistant_language
             self.__speaking.setup_assistant_voice()
 
     def change_language(self, assistant, *args: tuple):
         """
-        Изменение языка голосового ассистента (языка распознавания речи)
+        Changing the language of the voice assistant (speech recognition language)
         """
         assistant.speech_language = "ru" if assistant.speech_language == "en" else "en"
         self.__speaking.setup_assistant_voice()
@@ -184,19 +184,19 @@ class Making:
 
     def get_weather_forecast(self, person, translator, *args: tuple):
         """
-        Получение и озвучивание прогнза погоды
-        :param args: город, по которому должен выполняться запос
+        Getting and voicing the weather forecast
+        :param args: the city for which the zapos should be performed
         """
-        # в случае наличия дополнительного аргумента - запрос погоды происходит по нему,
-        # иначе - используется город, заданный в настройках
+        # if there is an additional argument , the weather request is made using it,
+        # otherwise, the city specified in the settings is used
         if args[0]:
             city_name = args[0][0]
         else:
             city_name = person.home_city
 
         try:
-            # использование API-ключа, помещённого в .env-файл по примеру WEATHER_API_KEY = "01234abcd....."
-            weather_api_key = os.getenv("WEATHER_API_KEY")
+            # using the API key placed in the. env file for example WEATHER_API_KEY = "01234abcd....."
+            weather_api_key = os.getenv("53e1dec3517a7f5e30251ccbb9049d18")
             open_weather_map = OWM(weather_api_key)
 
             # запрос данных о текущем состоянии погоды
@@ -204,26 +204,26 @@ class Making:
             observation = weather_manager.weather_at_place(city_name)
             weather = observation.weather
 
-        # поскольку все ошибки предсказать сложно, то будет произведен отлов с последующим выводом без остановки программы
+        # since it is difficult to predict all errors, it will be captured and then output without stopping the program
         except:
             self.__speaking.play_voice_assistant_speech(translator.get("Seems like we have a trouble. See logs for more information"))
             traceback.print_exc()
             return
 
-        # разбивание данных на части для удобства работы с ними
+        # splitting data into parts for the convenience of working with them
         status = weather.detailed_status
         temperature = weather.temperature('celsius')["temp"]
         wind_speed = weather.wind()["speed"]
-        pressure = int(weather.pressure["press"] / 1.333)  # переведено из гПА в мм рт.ст.
+        pressure = int(weather.pressure["press"] / 1.333)  # translated from gPA to mmHg.
 
-        # вывод логов
+        # output of logs
         print(colored("Weather in " + city_name +
                       ":\n * Status: " + status +
                       "\n * Wind speed (m/sec): " + str(wind_speed) +
                       "\n * Temperature (Celsius): " + str(temperature) +
                       "\n * Pressure (mm Hg): " + str(pressure), "yellow"))
 
-        # озвучивание текущего состояния погоды ассистентом (здесь для мультиязычности требуется дополнительная работа)
+        # voicing the current state of the weather by an assistant (additional work is required here for multilingualism)
         self.__speaking.play_voice_assistant_speech(translator.get("It is {0} in {1}").format(status, city_name))
         self.__speaking.play_voice_assistant_speech(translator.get("The temperature is {} degrees Celsius").format(str(temperature)))
         self.__speaking.play_voice_assistant_speech(translator.get("The wind speed is {} meters per second").format(str(wind_speed)))
